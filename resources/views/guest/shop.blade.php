639221a927e1b2160e9c1e56118d0898f587742c
unknown <moralesnmatias@gmail.com>
2022-02-07 20:21:10 -0300

    Creacion de layout con los navbar y footer y creacion product_shopController

--- resources/views/guest/shop.blade.php
+++ resources/views/guest/shop.blade.php
@@ -1,75 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <script src="https://cdn.tailwindcss.com/"></script>
-    <script src="chrome-extension://kgejglhpjiefppelpmljglcjbhoiplfn/shadydom.js"></script>
-    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
-    <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <section class="relative mx-auto">
-            <!-- navbar -->
-        <nav class="flex justify-between bg-gray-900 text-white w-screen">
-        <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-            <a class="text-3xl font-bold font-heading" href="#">
-                <!-- <img class="h-9" src="logo.png" alt="logo"> -->
-            Logo Here.
-            </a>
-                <!-- Nav Links -->
-            <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-            <li><a class="hover:text-gray-200" href="{{ route('experiencies.index') }}">Home</a></li>
-            
-            <li><a class="hover:text-gray-200" href="{{ route('experiencies.shop') }}">Shop</a></li>
-            <li><a class="hover:text-gray-200" href="#">Contact Us</a></li>
-            </ul>
-                <!-- Header Icons -->
-            <div class="hidden xl:flex  space-x-5 items-center">
-                <a class="hover:text-gray-200" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                </svg>
-                </a>
-                <a class="flex items-center hover:text-gray-200" href="cart_shop">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                <span class="flex absolute -mt-5 ml-4">
-                    <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                    <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-                    </span>
-                    </span>
-                </a>
-                <!-- Sign In / Register      -->
-                <a class="flex items-center hover:text-gray-200" href="account">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </a>
-                
-            </div>
-        </div>
-            <!-- Responsive navbar -->
-            <a class="xl:hidden flex mr-6 items-center" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            <span class="flex absolute -mt-5 ml-4">
-                <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-                </span>
-            </span>
-            </a>
-            <a class="navbar-burger self-center mr-12 xl:hidden" href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </a>
-        </nav>
-        </section>
-    </head>
+@extends('layouts.guest')
+@section('content')     
 <body class="overflow-x-hidden">
     <section class="relative  bg-blueGray-50">
         <!-- Main tittle with the photo -->
@@ -144,49 +74,28 @@
             <div tabindex="0" class="focus:outline-none border-b-2 border-gray-200 mb-5">
                 <!-- Remove py-8 -->
                 <div class="mx-auto container py-8">
+                                        
                     <div class="flex flex-wrap items-center lg:justify-between justify-center">
-                        <!-- Card 1 -->
+                        <!-- Card Categorias -->
                         @foreach ($category as $categories)
-                        <a class="cursor-pointer rounded-md shadow-md shadow-gray-200 hover:shadow-dark-400/80 hover:shadow-2xl hover:bg-gray-50" href="s">
-                            <div tabindex="0" class="focus:outline-none mx-2 w-72 xl:mb-0 mb-8">
-                                <div>
-                                    <img src="https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="">
+                            <a class="cursor-pointer rounded-md shadow-md shadow-gray-200 hover:shadow-dark-400/80 hover:shadow-2xl hover:bg-gray-50" 
+                                href="{{route('experiencies.product_shop',$categories)}}">
+                                <div tabindex="0" class="focus:outline-none mx-2 w-72 xl:mb-0 mb-8">
                                     <div>
-                                        <h1 class="mt-3 text-gray-800 text-2xl font-bold my-2"> {{ $categories->title }} </h1>
-                                        <p class="text-gray-700 mb-2"> {{ $categories->description }}</p>
-                                        <div class="flex justify-between mt-4">
-                                            <span class="font-thin text-sm">May 20th 2020</span>
-                                            <span class="mb-2 text-gray-800 font-bold">Read more</span>
+                                        <img src="https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="">
+                                        <div>
+                                            <h1 class="mt-3 text-gray-800 text-2xl font-bold my-2"> {{ $categories->title }} </h1>
+                                            <p class="text-gray-700 mb-2"> {{ $categories->description }}</p>
+                                            <div class="flex justify-between mt-4">
+                                                <span class="font-thin text-sm">May 20th 2020</span>
+                                                <span class="mb-2 text-gray-800 font-bold">Read more</span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </a>
-                            @endforeach
-                        <!-- Card 1 Ends -->
-                    </div>
-                    
-                    <div class="flex flex-wrap items-center lg:justify-between justify-center">
-                        <!-- Card 1 -->
-                        @foreach ($experiences as $experience)
-                        
-                        <a class="cursor-pointer rounded-md shadow-md shadow-gray-200 hover:shadow-dark-400/80 hover:shadow-2xl hover:bg-gray-50" href="{{route('guest.product',$experience)}}">
-                            <div tabindex="0" class="focus:outline-none mx-2 w-72 xl:mb-0 mb-8">
-                                <div>
-                                    <img src="https://images.unsplash.com/photo-1497493292307-31c376b6e479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="">
-                                    <div>
-                                        <h1 class="mt-3 text-gray-800 text-2xl font-bold my-2"> {{ $experience->title }} </h1>
-                                        <p class="text-gray-700 mb-2"> {{ $experience->description }}</p>
-                                        <div class="flex justify-between mt-4">
-                                            <span class="font-thin text-sm">May 20th 2020</span>
-                                            <span class="mb-2 text-gray-800 font-bold">Read more</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                            @endforeach
-                        <!-- Card 1 Ends -->
+                            </a>
+                        @endforeach
+                        <!-- Card Categorias Ends -->
                     </div> 
                 </div>  
             </div>
@@ -228,4 +137,4 @@
             </script>
     </section>
 </body>
-</html>
+@endsection 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 
 use App\Http\Controllers\ExperienceController;
 use App\Http\Controllers\GuestController;
+use app\Http\Controllers\Product_ShopController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Experience;
 use App\Models\Province;
@@ -12,6 +13,7 @@ use App\Models\Place;
 use App\Models\Languaje;
 use App\Models\User;
 use App\Models\Comment;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,6 +29,7 @@ Route::get('/',[ExperienceController::class,'index'])->name('experiencies.index'
 
 Route::get('/shop',[GuestController::class,'index'])->name('experiencies.shop');
 
+Route::get('/product_shop', [Product_ShopController::class,'index'])->name('experiencies.product_shop');
 
 Route::get('/product/{experience}', function (App\Models\Experience $experience) {
     $experiences = App\Models\Experience::all();
@@ -39,11 +42,7 @@ Route::get('/cart_shop', function () {
     return view('guest.cart_shop', compact(["experiences"]));
 });
 
-Route::get('/product_shop', function () {
-    $experiences = App\Models\Experience::all();
 
-    return view('guest.product_shop', compact(["experiences"]));
-});
 
 Route::middleware('auth')->get('/account', function () {
     $provinces=Province::all();
