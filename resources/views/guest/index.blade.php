3bf2a5fac98ede69535cd7c0d853902d897ee5f0
unknown <moralesnmatias@gmail.com>
2022-02-10 13:02:33 -0300

    Modificaciones de colores en index

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -143,31 +143,27 @@
               </p>
             </div>
             <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
-              <div
-                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg text-pink-600"
-              >
+              <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg text-gray-700">
                 <img
                   alt="..."
                   src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1051&amp;q=80"
                   class="w-full align-middle rounded-t-lg"
                 />
                 <blockquote class="relative p-8 mb-4">
-                  <svg
-                    preserveAspectRatio="none"
+                  <svg preserveAspectRatio="none"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 583 95"
                     class="absolute left-0 w-full block"
-                    style="height: 95px; top: -94px;"
-                  >
+                    style="height: 95px; top: -94px;">
                     <polygon
                       points="-30,95 583,95 583,65"
-                      class="text-pink-600 fill-current"
-                    ></polygon>
+                      class="text-gray-700 fill-current">
+                    </polygon>
                   </svg>
                   <h4 class="text-xl font-bold text-white">
                     Servicios de Primer Nivel
                   </h4>
-                  <p class="text-md font-light mt-2 text-white">
+                  <p class="text-md font-light mt-2 text-black">
                     Paseos en botes por los mejores ríos de Neuquén, guías turísticas por las calles de Buenos Aires,
                     Prueba los mejores vinos de Mendoza y muchísimos más.
                   </p>
