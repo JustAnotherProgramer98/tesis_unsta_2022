1335d126a4bcaefa4be6e702e907bae7ac83d712
vagrant <vagrant@homestead>
2022-03-06 05:02:55 +0000

    Error 404 y 405 maquetas arregladas

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.guest')
+
+@section('content')
+    <div class="bg-white">
+        <div class="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
+            <div class="bg-gray-100 sm:rounded-lg pb-8">
+                <div class="text-center p-8">
+                    <h1 class="text-9xl font-bold text-paleta_tesis_azul">405</h1>
+                    <h1 class="text-6xl font-medium py-8">Metodo no permitido</h1>
+                    <p class="text-lg pb-8 px-12 font-medium">La solicitud que estas intentando realizar no posee las creedenciales necesarias</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
